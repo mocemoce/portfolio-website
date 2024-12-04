d423ed3e58cde45e071f2eb920fe1c51a84f583b
Mocemoce <152973015+mocemoce@users.noreply.github.com>
2024-12-04 14:53:29 +0800

    1st commit
    Added the components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './components/Sidebar/Sidebar';
+import Services from './components/Services/Services';
+import Resume from './components/Resume/Resume';
+import Portfolio from './components/Portfolio/Portfolio';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Sidebar/>
+    <main className='main'>
+      <Home/>
+      <About/>
+      <Services/>
+      <Resume/>
+      <Portfolio/>
+      <Contact/>
+    </main>
+    </>
   );
 }
 

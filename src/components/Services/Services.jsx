34f715185afd12ed032476fb7376e1cad7cb3b9b
Mocemoce <152973015+mocemoce@users.noreply.github.com>
2024-12-08 19:14:55 +0800

    Services
    + assets

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,11 +1,50 @@
 import React from 'react';
 import "./Services.css";
+import Image1 from "../../assets/web-design.png"
+import Image2 from "../../assets/app-development.png"
+import Image3 from "../../assets/mobile-app.png"
 
+const data = [
+  {
+    id: 1,
+    image:  Image1,
+    title: "UI/UX Design",
+    description: 
+
+    "Proficient in Figma and Adobe Photoshop for UI/UX design, specializing in creating wireframes, prototypes, and high-fidelity visuals to ensure user-friendly and effective designs."  },
+  {
+    id: 2,
+    image:  Image2,
+    title: "Web Development",
+    description: 
+
+    "I specialize in front-end development using React, Angular, Bootstrap, and Tailwind CSS, building responsive, high-performance web apps with smooth UIs and cross-browser compatibility."  },
+  {
+    id: 1,
+    image:  Image3,
+    title: "Mobile Devlopment",
+    description: 
+    
+    "I have a solid understanding of Flutter, with experience building responsive, visually appealing Android apps, integrating APIs, managing state, and optimizing performance."  },
+]
 const Services = () => {
   return (
-    <div>
-      Services
-    </div>
+    <section className="services container section" id="services">
+      <h2 className="section_title">Services</h2>
+
+      <div className="services_container grid">
+        {data.map(({ id, image, title, description}) => {
+          return(
+            <div className="services_card" key={id}>
+            <img src={image} width="150px" alt="error" className="services_img" />
+
+            <h3 className="services_title">{title}</h3>
+            <p className="services_description">{description}</p>
+          </div>
+          )
+        })}
+      </div>
+    </section>
   )
 }
 
